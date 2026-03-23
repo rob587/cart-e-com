c07463c02ad20800312c658686a97e238c58c72d
rob587 <robertocamm02@yahoo.com>
2026-03-23 15:58:31 +0100

    Refactor App component to include routing and integrate DefaultLayout, Homepage, and Cart components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+//impostato routing tra le pagine
+
+import { CartProvider } from "./context/CartContext";
+import DefaultLayout from "./layout/DefaultLayout";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Cart from "./pages/Cart";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<Homepage />} />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+        </Routes>
+      </CartProvider>
+    </>
+  );
 }
 
 export default App;
